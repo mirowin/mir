46d335073868fb7e76faa7788bcc0956a279d5f7
mirowin <shitikov.n@gmail.com>
2016-06-09 20:23:21 +0300

    1

---
+++ translations/translations.js
@@ -0,0 +1,20 @@
+/* Magic Mirror
+ * Translation Definition
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+var translations = {
+	"en" : "translations/en.json", // English
+	"nl" : "translations/nl.json", // Dutch
+	"de" : "translations/de.json", // German
+	"fr" : "translations/fr.json", // French
+	"fy" : "translations/fy.json", // Frysk
+	"es" : "translations/es.json", // Spanish
+	"nb" : "translations/nb.json", // Norsk bokmål
+	"nn" : "translations/nn.json", // Norsk nynorsk
+	"pt" : "translations/pt.json", // Português
+	"sv" : "translations/sv.json", // Svenska
+	"it" : "translations/it.json", // Italian
+};
